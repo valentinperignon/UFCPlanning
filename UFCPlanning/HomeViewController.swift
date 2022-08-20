b071b861d61881f169ec177e56bbb0b447044bae
Valentin Perignon <valentin.perignon@ik.me>
2022-08-20 23:51:57 +0200

    Update project

--- UFCPlanning/HomeViewController.swift
+++ UFCPlanning/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  UFCPlanning
 //
 //  Created by Valentin Perignon on 20/08/2022.
@@ -7,13 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
