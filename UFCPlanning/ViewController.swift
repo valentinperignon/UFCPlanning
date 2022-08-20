65eb751dece7d44e5df20a35583d28f6ddc0a199
Valentin Perignon <valentin.perignon@ik.me>
2022-08-20 23:43:02 +0200

    Initial Commit

---
+++ UFCPlanning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UFCPlanning
+//
+//  Created by Valentin Perignon on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
