90b4c5807e7d675657fe0caa7ce33b0c4bdc75b9
Valentin Perignon <valentin.perignon@ik.me>
2022-08-26 21:22:14 +0200

    Update ApiFetcher

---
+++ UFCPlanningCore/Sources/UFCPlanningCore/Data/Models/Request Body/PlanningSettings.swift
@@ -0,0 +1,19 @@
+//
+//  PlanningSettings.swift
+//  
+//
+//  Created by Valentin Perignon on 26/08/2022.
+//
+
+import Foundation
+
+public struct PlanningSettings {
+    let id: Int
+    let numberOfDays: Int
+    let mode: Int
+    let withColors: Bool
+    let withSports: Bool
+    let withExtra: Bool
+    let studentId: Int
+    let token: String
+}
--- UFCPlanningCore/Sources/UFCPlanningCore/Data/Networking/ApiFetcher.swift
+++ UFCPlanningCore/Sources/UFCPlanningCore/Data/Networking/ApiFetcher.swift
@@ -15,11 +15,19 @@ public final class ApiFetcher {
     }
     
     public func trainings(with id: Int) async throws {
-        let _ = try await urlSession.data(endpoint: .trainings(with: 0))
+        let response = try await urlSession.data(endpoint: .trainings(with: 0))
         // TODO: Get trainings
     }
     
-    public func planning() {
+    public func planning(with settings: PlanningSettings) async throws {
+        let response = try await urlSession.data(endpoint: .planning(for: settings.id,
+                                                                     numberOdDays: settings.numberOfDays,
+                                                                     mode: settings.mode,
+                                                                     withColor: settings.withColors,
+                                                                     withSports: settings.withSports,
+                                                                     withExtra: settings.withExtra,
+                                                                     studentId: settings.studentId,
+                                                                     token: settings.token))
         // TODO: Get planning
     }
 }
