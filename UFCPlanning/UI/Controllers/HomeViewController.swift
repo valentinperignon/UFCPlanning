35b361bdea3f55eb2ff8367f0701c0d020dc5b62
Valentin Perignon <valentin.perignon@ik.me>
2022-09-06 22:39:51 +0200

    Update UI

--- UFCPlanning/UI/Controllers/HomeViewController.swift
+++ UFCPlanning/UI/Controllers/HomeViewController.swift
@@ -9,45 +9,46 @@ import RealmSwift
 import UFCPlanningCore
 import UIKit
 
-class HomeViewController: UITableViewController {
+class HomeViewController: UIViewController {
     private var planningManager: PlanningManager!
     
     private var observationToken: NotificationToken?
-    
     private var planning = AnyRealmCollection(List<Day>())
+    
+    private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "UFC Planning"
+        
+        navigationItem.title = "UFC Planning"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setUpTableView()
+        tableView.register(UINib(nibName: "SubjectCell", bundle: nil), forCellReuseIdentifier: "SubjectCell")
         
         planningManager = PlanningManager()
         observeDays()
+        
+        configureRefreshControl()
         
         Task {
             await fetchPlanning()
         }
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return planning.count
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let day = planning[section]
-        return day.date.formatted(.dateTime.day().month(.wide))
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let day = planning[section]
-        return day.subjects.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let day = planning[indexPath.section]
-        let subject = day.subjects[indexPath.row]
+    private func setUpTableView() {
+        tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+        view.addSubview(tableView)
         
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "\(subject.name) : \(subject.start.formatted(.dateTime.hour().minute()))"
-        return cell
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
     
     private func observeDays() {
@@ -59,10 +60,9 @@ class HomeViewController: UITableViewController {
             case let .initial(days):
                 self.planning = days
                 self.tableView.reloadData()
-            case let .update(days, deletions: deletions, insertions: insertions, modifications: modifications):
+            case let .update(days, _, _, _):
                 self.planning = days
                 self.tableView.reloadData()
-                // TODO: Update with batch action
             case let .error(error):
                 print("Error: \(error.localizedDescription)")
             }
@@ -75,5 +75,43 @@ class HomeViewController: UITableViewController {
         } catch {
             print("Error: \(error.localizedDescription)")
         }
+    }
+    
+    private func configureRefreshControl() {
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(refreshPlanning), for: .valueChanged)
+    }
+    
+    @objc private func refreshPlanning() {
+        Task {
+            await fetchPlanning()
+            tableView.refreshControl?.endRefreshing()
+        }
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return planning.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let day = planning[section]
+        return day.date.formatted(.dateTime.day().month(.wide))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let day = planning[section]
+        return day.subjects.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SubjectCell", for: indexPath) as! SubjectCell
+        
+        let day = planning[indexPath.section]
+        let subject = day.subjects[indexPath.row]
+        cell.configure(with: subject)
+        
+        return cell
     }
 }
--- UFCPlanning/UI/Views/SubjectCell.swift
+++ UFCPlanning/UI/Views/SubjectCell.swift
@@ -9,17 +9,21 @@ import UFCPlanningCore
 import UIKit
 
 class SubjectCell: UITableViewCell {
-    @IBOutlet weak var color: UIView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var about: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var border: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func configure(with subject: Subject) {
-        color.backgroundColor = subject.color
+        contentView.backgroundColor = subject.color.withAlphaComponent(0.05)
+        border.backgroundColor = subject.color
+        
         name.text = subject.name
         about.text = subject.about
+        time.text = "\(subject.start.formatted(.dateTime.hour().minute())) - \(subject.end.formatted(.dateTime.hour().minute()))"
     }
 }
