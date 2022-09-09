edaae00c8d25663df5535073f23b8ddb56f81005
Valentin Perignon <valentin.perignon@ik.me>
2022-09-09 21:38:48 +0200

    Transforms HomeViewController to UITableViewController

--- UFCPlanning/UI/Controllers/Home/HomeViewController.swift
+++ UFCPlanning/UI/Controllers/Home/HomeViewController.swift
@@ -9,68 +9,46 @@ import RealmSwift
 import UFCPlanningCore
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITableViewController {
     private var viewModel = HomeViewModel()
-    
-    private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "UFC Planning"
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        setUpTableView()
+
         tableView.register(UINib(nibName: "SubjectCell", bundle: nil), forCellReuseIdentifier: "SubjectCell")
 
         configureRefreshControl()
-    }
-    
-    private func setUpTableView() {
-        tableView = UITableView(frame: view.frame, style: .insetGrouped)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.dataSource = self
-        tableView.allowsSelection = false
-        view.addSubview(tableView)
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
     }
     
     private func configureRefreshControl() {
         tableView.refreshControl = UIRefreshControl()
         tableView.refreshControl?.addTarget(viewModel, action: #selector(viewModel.refreshPlanning), for: .valueChanged)
     }
-}
 
-// MARK: - UITableViewDataSource
-
-extension HomeViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return viewModel.planning.count
     }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let day = viewModel.planning[section]
         return day.date.formatted(.dateTime.day().month(.wide))
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let day = viewModel.planning[section]
         return day.subjects.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SubjectCell", for: indexPath) as! SubjectCell
-        
+
         let day = viewModel.planning[indexPath.section]
         let subject = day.subjects[indexPath.row]
         cell.configure(with: subject)
-        
+
         return cell
     }
 }
