74197f8ba47b7ca40fd295eaf1f999024323229f
Valentin Perignon <valentin.perignon@ik.me>
2022-09-13 10:50:33 +0200

    Settings : add sections and items

--- UFCPlanning/UI/Controllers/Settings/SettingsViewController.swift
+++ UFCPlanning/UI/Controllers/Settings/SettingsViewController.swift
@@ -37,4 +37,58 @@ extension SettingsViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return viewModel.settings[section].name
     }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if viewModel.settings[section] == SettingsSection.plannings {
+            return 1
+        }
+        return viewModel.settings[section].items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = viewModel.settings[indexPath.section]
+        switch section {
+        case .account:
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Aucun compte connecté"
+            return cell
+        case .plannings:
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Ajouter un planning"
+            return cell
+        case .about:
+            let item = section.items[indexPath.row]
+            let cell = UITableViewCell()
+            cell.textLabel?.text = item.name
+            return cell
+        default:
+            fatalError("Unhandled section")
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SettingsViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let section = viewModel.settings[indexPath.section]
+        switch section {
+        case .account:
+            let alertVC = UIAlertController(title: "Se connecter", message: nil, preferredStyle: .alert)
+            alertVC.addTextField { textField in
+                textField.placeholder = "Identifiant"
+            }
+            alertVC.addTextField { textField in
+                textField.placeholder = "Mot de passe"
+                textField.isSecureTextEntry = true
+            }
+            alertVC.addAction(UIAlertAction(title: "Valider", style: .default))
+            alertVC.addAction(UIAlertAction(title: "Annuler", style: .cancel))
+            present(alertVC, animated: true)
+        default:
+            fatalError("Unhandled section") 
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
--- UFCPlanning/UI/Controllers/Settings/SettingsViewModel.swift
+++ UFCPlanning/UI/Controllers/Settings/SettingsViewModel.swift
@@ -9,21 +9,21 @@ import Foundation
 import SFSafeSymbols
 import UIKit
 
-class SettingsSection {
+struct SettingsSection: Equatable {
+    let id: Int
     let name: String
-    var items: [SettingsItem]
+    let items: [SettingsItem]
 
-    static let account = SettingsSection(name: "Compte", items: [.account])
-    static let plannings = SettingsSection(name: "Plannings", items: [])
-    static let about = SettingsSection(name: "À Propos", items: [.visibility, .campusSport, .homeworkAlerts])
+    static let account = SettingsSection(id: 0, name: "Compte", items: [.account])
+    static let plannings = SettingsSection(id: 1, name: "Plannings", items: [.plannings])
+    static let about = SettingsSection(id: 2, name: "Autres", items: [.visibility, .campusSport, .homeworkAlerts])
 
-    init(name: String, items: [SettingsItem]) {
-        self.name = name
-        self.items = items
+    static func == (lhs: SettingsSection, rhs: SettingsSection) -> Bool {
+        return lhs.id == rhs.id
     }
 }
 
-class SettingsItem {
+struct SettingsItem {
     let icon: SFSymbol
     let name: String
 
@@ -34,11 +34,6 @@ class SettingsItem {
     static let visibility = SettingsItem(icon: .eyes, name: "Visibilité")
     static let campusSport = SettingsItem(icon: .figureWalk, name: "Activités Campus Sport")
     static let homeworkAlerts = SettingsItem(icon: .bell, name: "Alertes pour les devoirs")
-
-    init(icon: SFSymbol, name: String) {
-        self.icon = icon
-        self.name = name
-    }
 }
 
 class SettingsViewModel {
